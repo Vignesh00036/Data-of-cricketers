a1d615ce0fa014c39f54da856d1fb3916646d896
Vignesh Elumalai <vigneshoffl36@gmail.com>
2024-10-19 12:40:30 +0530

    Update data_aws_s3.py

--- Program Files/data_aws_s3.py
+++ Program Files/data_aws_s3.py
@@ -7,14 +7,15 @@ files =['players_information','odi','test','t20','ipl']
 
 def upload(team_name):
     replaced_team_name=team_name.replace(' ', '_')
-    path=f'/media/beast/Beast/DE/Python_programms/1_OG/data/{replaced_team_name}'
+    path={your_file_path}
     if os.path.exists(path):
         for idx,i in enumerate(os.listdir(path)):
-            file_path=f'/media/beast/Beast/DE/Python_programms/1_OG/data/{replaced_team_name}/{files[idx]}_records.csv'
-            bucket_name='s3-bucket-for-data'
-            s3_path=f'{files[idx]}_records/{files[idx]}_{team_name}.csv'
+            file_path={your_file_path/files[idx].csv}
+            bucket_name={s3_bucket_name}
+            s3_path={your_s3_path}
             s3.upload_file(file_path, bucket_name, s3_path)
 
+# Method -2
 # def uploading_to_s3(team_name):
 #     for file in files:
 #         upload_statement=[
