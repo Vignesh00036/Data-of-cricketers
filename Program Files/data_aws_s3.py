3bf05b19640b165f70fe53d0d8a541b25b56d92a
Vignesh Elumalai <vigneshoffl36@gmail.com>
2024-10-19 12:50:33 +0530

    Update data_aws_s3.py

--- Program Files/data_aws_s3.py
+++ Program Files/data_aws_s3.py
@@ -2,9 +2,11 @@ import os
 import boto3
 # import subprocess
 
+#before this process you should configure aws in your system
 s3=boto3.client('s3')
 files =['players_information','odi','test','t20','ipl']
 
+# this function is going to upload all files including folders into s3 bucket
 def upload(team_name):
     replaced_team_name=team_name.replace(' ', '_')
     path={your_file_path}
@@ -16,6 +18,7 @@ def upload(team_name):
             s3.upload_file(file_path, bucket_name, s3_path)
 
 # Method -2
+# uploading files using aws cli
 # def uploading_to_s3(team_name):
 #     for file in files:
 #         upload_statement=[
