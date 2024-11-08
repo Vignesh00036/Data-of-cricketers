0a5f1e2bc29eb572facda07519af65d71e469e86
Vignesh Elumalai <vigneshoffl36@gmail.com>
2024-11-08 13:22:21 +0530

    Update data_snowflake.py

--- Program Files/data_snowflake.py
+++ Program Files/data_snowflake.py
@@ -10,10 +10,30 @@ conn = snowflake.connector.connect(
     database='your_database',
     schema='your_schema',
 )
-matches=['ODI_RECORDS','TEST_RECORDS','T20_RECORDS','IPL_RECORDS']
-cursor=conn.cursor()
+cursor= conn.cursor()
 
-# Creates tables for match formats
+#creating a storage integration
+create_storage_integration=""" 
+		CREATE STORAGE INTEGRATION IF NOT EXISTS MY_INTEGRATION
+		TYPE=EXTERNAL_STAGE
+		STORAGE_PROVIDER=S3
+		ENABLED=TRUE
+		STORAGE_AWS_ROLE_ARN='arn:aws:iam::597088026789:role/snowflake-role'
+		STORAGE_ALLOWED_LOCATIONS=('s3://s3-bucket-for-data/'); 
+	"""
+cursor.execute(create_storage_integration)
+
+#creating a file format
+create_file_format=""" 
+		CREATE FILE FORMAT IF NOT EXISTS MY_FORMAT
+		TYPE = 'CSV' 
+		FIELD_DELIMITER = ',';
+ """
+cursor.execute(create_file_format)
+
+matches=['ODI_RECORDS','TEST_RECORDS','T20_RECORDS','IPL_RECORDS']
+
+#creating tables
 for match in matches:
 	create_statement_for_records = f""" 
 	CREATE TABLE IF NOT EXISTS {match}(
@@ -36,8 +56,7 @@ for match in matches:
 		);
 	"""
 	cursor.execute(create_statement_for_records)
-
-# Creates table for players_information_records
+	
 create_statement_for_players_info=""" 
         CREATE TABLE IF NOT EXISTS PLAYERS_INFORMATION_RECORDS(
             ID int unique autoincrement,
@@ -51,8 +70,46 @@ create_statement_for_players_info="""
             PRIMARY KEY (id));
 	"""
 cursor.execute(create_statement_for_players_info)
-conn.commit()
 
+#creating stages
+for stage in matches:
+	create_stmt_for_stage=f""" 
+			CREATE STAGE IF NOT EXISTS {stage}_stage
+			URL='s3://s3-bucket-for-data/{stage}/'
+			STORAGE_INTEGRATION=MY_INTEGRATION
+			COMMENT='This is my external storage for aws';
+	 """
+	cursor.execute(create_stmt_for_stage)
+
+create_stmt_for_players_stage=f""" 
+			CREATE STAGE IF NOT EXISTS players_information_stage
+			URL='s3://s3-bucket-for-data/players_information_records/'
+			STORAGE_INTEGRATION=MY_INTEGRATION
+			COMMENT='This is my external storage for aws';
+	 """
+cursor.execute(create_stmt_for_stage)
+
+
+#creating pipes
+for pipe in matches:
+	create_pipe=f""" 
+		CREATE PIPE IF NOT EXISTS {pipe}_pipe
+		AUTO_INGEST=TRUE AS
+			COPY INTO {pipe}
+			FROM @{pipe}_stage
+			FILE_FORMAT = MY_FORMAT; 
+	"""
+	cursor.execute(create_pipe)
+
+create_pipe_for_players=f""" 
+		CREATE PIPE IF NOT EXISTS players_pipe
+		AUTO_INGEST=TRUE AS
+			COPY INTO PLAYERS_INFORMATION_RECORDS
+			FROM @players_information_stage
+			FILE_FORMAT = MY_FORMAT; 
+	"""
+cursor.execute(create_pipe_for_players)
+conn.commit()
 
 # this function imports all files into snowflake internal stage
 def importing_files(team_name):
